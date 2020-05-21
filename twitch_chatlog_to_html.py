65cf1a42f1200faa02e7584e6be67bcb4315e0ac
TimZF <timziegler99@gmail.com>
2020-05-21 12:27:03 +0200

    Change format to replace
    with python 3.8.3 the format will throw an exception because it sees { in the css. Not the best solution but it works atleast for me.

--- twitch_chatlog_to_html.py
+++ twitch_chatlog_to_html.py
@@ -59,7 +59,8 @@ def messageContainer(username,content,user_color):
 
 def intoTemplate(filename,content):
 	with codecs.open(filename, encoding='utf-8') as f:
-		return f.read().format(content);
+		htmlSite = f.read()
+		return htmlSize.replace("{0}", content)
 
 def findEmote(word,full = True):
 	if isinstance(word,re.Match): 
